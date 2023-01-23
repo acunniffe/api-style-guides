958084f7de705865b2182d3d256782a360c456d3
Aidan Cunniffe <acunniffe@gmail.com>
2023-01-23 14:43:34 -0500

    remove oas config option

--- standards/index.ts
+++ standards/index.ts
@@ -1,7 +1,6 @@
 import {StandardDefinition} from "./lib/standard-definition";
 
 export const allStandards: Record<string, StandardDefinition<any>> =  {
-  OAS_CORRECTNESS: require('./oas-correctness').default,
   OpticBreakingChanges: require('./breaking-changes').default,
   UrlStyleGuide: require('./url-style-guides').default,
   Naming: require('./naming').default,
